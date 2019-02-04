37d9cfa6ab35b3d2de633c1f0d46f64cedf0e404
Leo Gisselberg <leo.gisselberg@voiapp.io>
2019-02-04 22:09:05 +0100

    Get detailed information async now working

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 import express from 'express'
 import { apiKey, randomOrg } from '../apiKey'
-import { placesNearby } from '@google/maps'
 import bodyParser from 'body-parser'
+import { resolve } from 'path';
 
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
 
@@ -21,26 +21,33 @@ const googleMapsClient = require('@google/maps').createClient({
 // Find restaurant in area and randomly choose one
 app.post('/find/:latlng', (req, res) => {
   const keyword = req.body.keywords[Math.floor(Math.random() * req.body.keywords.length)]
-  
   googleMapsClient.placesNearby({
-    location: req.params.latlng, 
-    radius: parseInt(req.body.radius), 
+    location: req.params.latlng,
+    radius: req.body.radius,
     keyword,
     type: 'restaurant'
-  })
-  .asPromise()
-  .then(response => {
+  }).asPromise().then(async (response) => {
     const a = response.json.results
     // Add random.org api instead of math
     const result = a[Math.floor(Math.random() * a.length)]
+    const details = await getDetailedInfo(result.place_id)
     res.send({
-      result
+      details
     })
-  })
-  .catch((err) => {
-    console.log(err)
+  }).catch(e => {
+    console.log(e)
   })
 })
+
+const getDetailedInfo = async (id) => {
+  return googleMapsClient.place({
+    placeid: id
+  }).asPromise().then(res => {
+    return res
+  }).catch((e) => {
+    console.log(e)
+  })
+}
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server running on port ${port}`))
